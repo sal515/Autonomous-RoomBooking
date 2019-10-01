4da7947d745cd83041b938c07d13fa23c4e43648
Salman <rahman_5@live.ca>
2019-09-30 23:25:54 -0400

    Server: Added functions to get hostname; updated printf to cout;

--- Server/server_app/server_app/server_app.cpp
+++ Server/server_app/server_app/server_app.cpp
@@ -4,83 +4,121 @@
 #include "pch.h"
 #include <iostream>
 
-#include "stdio.h"
+#include <cstdio>
 #include <winsock2.h>
-#pragma comment(lib,"ws2_32.lib") //Winsock Library
-#include <ws2tcpip.h>
+#pragma comment(lib,"ws2_32.lib") //Linking Winsock Library
+#include <ws2tcpip.h>	// Used to convert IPv4 or IPv6 addressed to standard binary and vice versa
+
+#include <string>
 
 #define BUFLEN 512  //Max length of buffer
 #define PORT 8888   //The port on which to listen for incoming data
 
 int main()
 {
+	/*
+	 * Socket Variables
+	 */
+
+	WSADATA win_socket_struct;
 	SOCKET s;
-	struct sockaddr_in server, si_other;
-	int slen, recv_len;
+	struct sockaddr_in server_struct;
+
 	char buf[BUFLEN];
-	WSADATA wsa;
-	slen = sizeof(si_other);
-	
+	std::string buffer;
+	struct sockaddr_in client_struct;
+	int client_struct_len, recv_len;
+	client_struct_len = sizeof(client_struct);
+	/*
+	 */
+
+
+	/*
+	 * Other variables (unrelated to sockets)
+	 */
+	std::string server_hostname;
+	std::string clientIP;
+	std::string client_hostname;
+
+	/*
+	 */
+
 	//Initialise winsock
-	printf("\nInitialising Winsock...");
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	std::cout << "\nInitialising Winsock... " << std::endl;
+
+	if (WSAStartup(MAKEWORD(2, 2), &win_socket_struct) != 0)
 	{
-		printf("Failed. Error Code : %d", WSAGetLastError());
+		std::cout << "Failed. Error Code : " << WSAGetLastError() << std::endl;
 		exit(EXIT_FAILURE);
 	}
 
-	printf("Initialised.\n");
-	
+	std::cout << "Initialised Winsock" << std::endl;
+
+
+	char c_hostname[128];
+	if ((gethostname(c_hostname, sizeof c_hostname) != 0))
+	{
+		std::cout << "Error while retrieving hostname.Error Code : " << WSAGetLastError() << std::endl;
+	}
+
+	if (c_hostname != nullptr)
+	{
+		server_hostname = std::string(c_hostname);
+		std::cout << "Hostname of computer is: " << server_hostname << std::endl;
+	}
+
 	//Create a socket
 	if ((s = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
 	{
-		printf("Could not create socket : %d", WSAGetLastError());
+		std::cout << "Could not create socket : " << WSAGetLastError << std::endl;
 	}
 
-	printf("Socket created.\n");
-	
-	//Prepare the sockaddr_in structure
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons(PORT);
+	std::cout << "Socket created" << std::endl;
 
-	//Bind
-	if (bind(s, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
+	//Prepare the sockaddr_in structure
+	server_struct.sin_family = AF_INET;
+	server_struct.sin_addr.s_addr = INADDR_ANY;
+	server_struct.sin_port = htons(PORT);
+
+	//Bind Socket
+	if (bind(s, (struct sockaddr *)&server_struct, sizeof(server_struct)) == SOCKET_ERROR)
 	{
-		printf("Bind failed with error code : %d", WSAGetLastError());
+		std::cout << "Bind failed with error code : " << WSAGetLastError << std::endl;
 		exit(EXIT_FAILURE);
 	}
-	puts("Bind done");
+
+	std::cout << "Bind done" << std::endl;
 
 	//keep listening for data
 	while (1)
 	{
-		printf("Waiting for data...\n");
+		std::cout << "Waiting for data..." << std::endl;
 		fflush(stdout);
 
 		//clear the buffer by filling null, it might have previously received data
 		memset(buf, '\0', BUFLEN);
 
-		if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == SOCKET_ERROR)
+		if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *)&client_struct, &client_struct_len)) == SOCKET_ERROR)
 		{
-			printf("recvfrom() failed with error code : %d", WSAGetLastError());
+			std::cout << "recvfrom() failed with error code : " << WSAGetLastError << std::endl;
 			exit(EXIT_FAILURE);
 		}
 
+		buffer = std::string(buf);
+
 		//print details of the client/peer and the data received
-		// printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+		char printable_IP_add_buf[INET_ADDRSTRLEN];
+		if ((inet_ntop(AF_INET, &client_struct.sin_addr, printable_IP_add_buf, INET_ADDRSTRLEN)) != nullptr)
+		{
+			clientIP = std::string(printable_IP_add_buf);
+		}
 
-		char pStringBuf[INET_ADDRSTRLEN];
-
-		inet_ntop(AF_INET, &si_other.sin_addr, pStringBuf, INET_ADDRSTRLEN);
-
-
-		printf("Data Recieved from client: %s\n", buf);
+		std::cout << "Data Recieved from client IP - " << clientIP << ": " << buffer << std::endl;
 
 		//now reply the client with the same data
-		if (sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == SOCKET_ERROR)
+		if (sendto(s, buf, recv_len, 0, (struct sockaddr*)&client_struct, client_struct_len) == SOCKET_ERROR)
 		{
-			printf("sendto() failed with error code : %d", WSAGetLastError());
+			std::cout << "sendto() failed with error code : " << WSAGetLastError << std::endl;
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -90,5 +128,3 @@ int main()
 
 	return 0;
 }
-
-
