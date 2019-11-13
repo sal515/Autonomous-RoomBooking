e052c53b35fa68aee909175a88374be643dfe528
Salman <salman.rahman515@gmail.com>
2019-11-12 20:49:39 -0500

    Server: receiving and parsing json data + removed protobuf from server.cpp and

--- Server/server_app/server_app/server_app.cpp
+++ Server/server_app/server_app/server_app.cpp
@@ -1,8 +1,4 @@
 // server_app.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-// Generate proto files from the proto file definition:
-// ..\..\..\protoc - 3.10.1 - win32\bin\protoc.exe - I = ..\..\..\Proto\ --cpp_out =. ..\..\..\Proto\messages.proto
 
 #include "pch.h"
 #include <iostream>
@@ -12,19 +8,43 @@
 #pragma comment(lib,"ws2_32.lib") //Linking Winsock Library
 #include <ws2tcpip.h>	// Used to convert IPv4 or IPv6 addressed to standard binary and vice versa
 
+// Other libraries
 #include <string>
-#include <algorithm>
-#include "messages.pb.h"
+#include "json.hpp"
+#include <vector>
+
+#include "dbHelper.h"
+
+#include <windows.h>
+// #include <fileapi.h>
+
+
+// namespaces
+using json = nlohmann::json;
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::fstream;
+using std::vector;
+
 
 // Note: If I want to send x characters my buff has to be x+1 for '\0' character at the end
 #define BUFLEN 32768		//Max length of buffer 
 #define PORT 8888   //The port on which to listen for incoming data
 
+
+// Path for directories and files
+const auto dir_local_storage = "local_storage";
+const auto log_path = "local_storage/log.json";
+const auto db_path = "local_storage/db.json";
+const auto example_db_path = "local_storage/example_db.json";
+
 int main(void)
 {
-	// Verify that the version of the library that we linked against is
-	// compatible with the version of the headers we compiled against.
-	// GOOGLE_PROTOBUF_VERIFY_VERSION;
+	dbHelper::createDirectory(dir_local_storage);
+	json db = dbHelper::db_to_json(db_path);
+
 
 	/*
 	 * Socket Variables
@@ -32,26 +52,26 @@ int main(void)
 
 	WSADATA win_socket_struct;
 	SOCKET s;
-	struct sockaddr_in server_struct;
+
+	struct sockaddr_in server_struct, si_other;
+
+	int server_struct_len, recv_len;
+	server_struct_len = sizeof(server_struct);
+
 
 	char buf[BUFLEN];
 	std::string buffer;
-	struct sockaddr_in client_struct;
-	int client_struct_len, recv_len;
-	client_struct_len = sizeof(client_struct);
-	/*
-	 */
 
 
-	/*
-	 * Other variables (unrelated to sockets)
-	 */
-	std::string server_hostname;
+	// /*
+	//  * Other variables (unrelated to sockets)
+	//  */
+	// std::string server_hostname;
 	std::string clientIP;
-	std::string client_hostname;
-
-	/*
-	 */
+	// std::string client_hostname;
+	//
+	// /*
+	//  */
 
 	//Initialise winsock
 	std::cout << "\nInitialising Winsock... " << std::endl;
@@ -65,22 +85,22 @@ int main(void)
 	std::cout << "Initialised Winsock" << std::endl;
 
 
-	char c_hostname[128];
-	if ((gethostname(c_hostname, sizeof c_hostname) != 0))
-	{
-		std::cout << "Error while retrieving hostname.Error Code : " << WSAGetLastError() << std::endl;
-	}
-
-	if (c_hostname != nullptr)
-	{
-		server_hostname = std::string(c_hostname);
-		std::cout << "Hostname of computer is: " << server_hostname << std::endl;
-	}
+	// char c_hostname[128];
+	// if ((gethostname(c_hostname, sizeof c_hostname) != 0))
+	// {
+	// 	std::cout << "Error while retrieving hostname.Error Code : " << WSAGetLastError() << std::endl;
+	// }
+	//
+	// if (c_hostname != nullptr)
+	// {
+	// 	server_hostname = std::string(c_hostname);
+	// 	std::cout << "Hostname of computer is: " << server_hostname << std::endl;
+	// }
 
 	//Create a socket
 	if ((s = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
 	{
-		std::cout << "Could not create socket : " << WSAGetLastError << std::endl;
+		std::cout << "Could not create socket : " << WSAGetLastError() << std::endl;
 	}
 
 	std::cout << "Socket created" << std::endl;
@@ -93,7 +113,7 @@ int main(void)
 	//Bind Socket
 	if (bind(s, (struct sockaddr *)&server_struct, sizeof(server_struct)) == SOCKET_ERROR)
 	{
-		std::cout << "Bind failed with error code : " << WSAGetLastError << std::endl;
+		std::cout << "Bind failed with error code : " << WSAGetLastError() << std::endl;
 		exit(EXIT_FAILURE);
 	}
 
@@ -108,31 +128,26 @@ int main(void)
 		//clear the buffer by filling null, it might have previously received data
 		memset(buf, '\0', BUFLEN + 1);
 
-		if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *)&client_struct, &client_struct_len)) ==
+		if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *)&server_struct, &server_struct_len)) ==
 			SOCKET_ERROR)
 		{
-			std::cout << "recvfrom() failed with error code : " << WSAGetLastError << std::endl;
+			std::cout << "recvfrom() failed with error code : " << WSAGetLastError() << std::endl;
 			exit(EXIT_FAILURE);
 		}
 
-		messages::request request;
-
-		std::cout << strlen(buf) << std::endl;
-		int sizeOfReceivedMsg = strlen(buf);
-
-		// buffer = std::string(buf);
-
-		std::cout << sizeOfReceivedMsg << std::endl;
-		request.ParseFromArray(buf, sizeOfReceivedMsg);
-
-		std::cout << "Received data:" << request.topic() << std::endl;
-
 		//print details of the client/peer and the data received
-		// char printable_IP_add_buf[INET_ADDRSTRLEN];
-		// if ((inet_ntop(AF_INET, &client_struct.sin_addr, printable_IP_add_buf, INET_ADDRSTRLEN)) != nullptr)
-		// {
-		// 	clientIP = std::string(printable_IP_add_buf);
-		// }
+		char printable_IP_add_buf[INET_ADDRSTRLEN];
+		if ((inet_ntop(AF_INET, &server_struct.sin_addr, printable_IP_add_buf, INET_ADDRSTRLEN)) != nullptr)
+		{
+			clientIP = std::string(printable_IP_add_buf);
+		}
+
+		cout << "clientIP: " << clientIP << endl;
+		buffer = std::string(buf);
+		json received_data = json::parse(buffer);
+		cout << received_data.at("message") << std::endl;
+
+
 		//
 		// std::cout << "Data Recieved from client IP - " << clientIP << ": " << buffer << std::endl;
 
