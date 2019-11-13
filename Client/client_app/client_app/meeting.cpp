3dadca2e40e5f7a1b4083c900160f081fc5c4ef2
Salman <salman.rahman515@gmail.com>
2019-11-13 02:58:31 -0500

    Client: Completed the logic to map meeting structure to JSON - Needs Testing

--- Client/client_app/client_app/meeting.cpp
+++ Client/client_app/client_app/meeting.cpp
@@ -7,12 +7,34 @@ json meeting::meetingObj_to_json(const meeting& meetInfo)
 	meeting_json["minimumParticipants"] = meetInfo.minimumParticipants;
 	meeting_json["rq"] = meetInfo.rq;
 	meeting_json["mt"] = meetInfo.mt;
-	meeting_json["invitedParticipants"] = meetInfo.invitedParticipants;
-	meeting_json["confirmedParticipants"] = meetInfo.confirmedParticipants;
+
+	json invitationParticipantArr = json::array();
+	for (user element : meetInfo.invitedParticipants)
+	{
+		invitationParticipantArr.push_back
+		({
+				{"ip", element.ip},
+				{"listeningPort", element.listeningPort},
+				{"userName", element.userName}
+			});
+	}
+	meeting_json["invitedParticipants"] = invitationParticipantArr;
+
+	json confirmedParticipantArr = json::array();
+	for (user element : meetInfo.confirmedParticipants)
+	{
+		confirmedParticipantArr.push_back
+		({
+				{"ip", element.ip},
+				{"listeningPort", element.listeningPort},
+				{"userName", element.userName}
+			});
+	}
+	meeting_json["confirmedParticipants"] = confirmedParticipantArr;
 	meeting_json["roomNumber"] = meetInfo.roomNumber;
 	meeting_json["topic"] = meetInfo.topic;
-	meeting_json["requester"] = 
-	{ 
+	meeting_json["requester"] =
+	{
 		{"ip", meetInfo.requester.ip},
 		{"listeningPort", meetInfo.requester.listeningPort},
 		{"userName", meetInfo.requester.userName}
