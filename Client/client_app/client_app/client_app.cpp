0fb4f52fc79d6d0351bfab909a83f00a92766658
AidaBumbu <aida.bumbu@gmail.com>
2019-11-13 18:24:37 -0500

    Book a room menu

--- Client/client_app/client_app/client_app.cpp
+++ Client/client_app/client_app/client_app.cpp
@@ -13,6 +13,9 @@
 
 // Other libraries
 #include <string>
+#include <ctime>
+#include <sstream>
+#include <iomanip>
 #include "json.hpp"
 #include <vector>
 
@@ -46,11 +49,14 @@ const auto example_db_path = "local_storage/example_db.json";
 
 // Example Function prototypes
 int test_pause_exit();
+void menu();
+bool check_schedule(struct tm *time);
+bool extract_date(const std::string& s, int& d, int& m, int& y);
 
 
 int main(void)
 {
-	dbHelper::createDirectory(dir_local_storage);
+	/*dbHelper::createDirectory(dir_local_storage);
 
 	json db = dbHelper::db_to_json(db_path);
 
@@ -101,7 +107,7 @@ int main(void)
 
 
 	//start communication
-	while (1)
+	while (true)
 	{
 		// cout << "Enter message : ";
 		// cin >> message;
@@ -142,11 +148,182 @@ int main(void)
 	}
 
 	closesocket(s);
-	WSACleanup();
+	WSACleanup(); */
 
-	return 0;
+	cout << "This is supposed to output";
+
+	return test_pause_exit();
 }
 
+
+void menu() 
+{ 
+	char choice;
+	cout << "Please select from the following options:\n"
+		 << "b. Book a Room\n"
+		 << "c. Cancel a Meeting\n"
+		 << "w. Withdraw from a Meeting\n"
+		 << "r. Request to Participate\n"
+		 << "e. Exit the Program\n";
+
+	cin >> choice;
+
+	switch (choice)
+	{
+	case 'b':
+		string date_time;
+		int mm, dd, yyyy, hh, min_participants;
+		string topic;
+		cout << "Please provide the following details:\n"
+			<< "Date (DD/MM/YYYY): ";
+		cin >> date_time;
+		while (!extract_date(date_time, dd, mm, yyyy))
+		{
+			cout << "\nPlease input a valid date (DD/MM/YYYY): ";
+			cin >> date_time;
+		}
+		cout << "\nTime of meeting (24hr): ";
+		cin >> hh;
+		while (hh > 24 || hh < 0)
+		{
+			cout << "\nPlease input a valid time 0-24: ";
+			cin >> hh;
+		}
+		cout << "\nTopic of meeting: ";
+		cin >> topic;
+		cout << "\nMinimum number of Participants: ";
+		cin >> min_participants;
+		vector<string> participants = list_of_participants(min_participants);
+
+		//Elie copy into json to send to server + implement request id?
+		break;
+	
+
+	}
+
+
+
+}
+
+vector<string> list_of_participants(int min) {
+	vector<string> participants;
+	vector<string>::iterator it;
+	string participants_menu = " a. add new participant\n v. view participants\n f. finish\n";
+	string ip;
+	char choice = 'm';
+	while (choice != 'e') 
+	{
+		cin >> choice;
+		switch (choice)
+		{
+		case 'm': //show menu
+			cout << participants_menu;
+			cin >> choice;
+			break;
+		case 'a': //add participants
+			
+			cout << "IP: ";
+			cin >> ip;
+			if (participants.empty())
+			{
+				if (check_ip(ip))
+				{
+					participants.push_back(ip);
+				}
+				else
+				{
+					cout << "IP address is not valid.\n";
+					choice = 'm';
+				}
+			}
+			else
+			{
+				it = find(participants.begin(), participants.end(), ip);
+				if (it != participants.end())
+				{
+					cout << "Participant is already part of the list\n";
+				}
+				else
+				{
+					if (check_ip(ip))
+					{
+						participants.push_back(ip);
+					}
+					else
+					{
+						cout << "IP address is not valid.\n";
+						choice = 'm';
+					}
+				}
+
+			} 
+			choice = 'm';
+			break;
+		case 'v': //output list of participants
+			for (int i = 0; i < participants.size(); i++) 
+			{
+				cout << participants.at(i);
+			}
+			choice = 'm';
+			break;
+		case 'f': //check if list is complete
+			if (min < participants.size()) 
+			{
+				choice = 'e';
+			}
+			else 
+			{
+				cout << "The total number of participants is smaller than the minimum required.\nYou need to add "
+					<< min - participants.size() << " more participants.";
+				choice = 'm';
+			}
+		default :
+			cout << "Please input a valid choice.\n";
+			choice = 'm';	
+		}
+
+	}
+
+}
+
+
+
+bool check_ip(const string &ip)
+{
+	struct sockaddr_in sa;
+	int result = inet_pton(AF_INET, ip.c_str(), &(sa.sin_addr));
+	return result != 0;
+}
+
+// function expects the string in format dd/mm/yyyy:
+bool extract_date(const std::string& s, int& d, int& m, int& y) 
+{
+	std::istringstream is(s);
+	char delimiter;
+	if (is >> d >> delimiter >> m >> delimiter >> y) 
+	{
+		struct tm t = { 0 };
+		t.tm_mday = d;
+		t.tm_mon = m - 1;
+		t.tm_year = y - 1900;
+		t.tm_isdst = -1;
+
+		// normalize:
+		time_t when = mktime(&t);
+		const struct tm *norm = localtime(&when);
+		// the actual date would be:
+		// m = norm->tm_mon + 1;
+		// d = norm->tm_mday;
+		// y = norm->tm_year;
+		// e.g. 29/02/2013 would become 01/03/2013
+
+		// validate (is the normalized date still the same?):
+		return (norm->tm_mday == d &&
+			norm->tm_mon == m - 1 &&
+			norm->tm_year == y - 1900);
+	}
+	return false;
+}
 
 // ==================  Examples  ======================================
 int test_pause_exit()
