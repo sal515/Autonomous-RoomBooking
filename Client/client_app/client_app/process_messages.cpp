c39e93253e96feae299d480012cda42fb803912d
Salman <salman.rahman515@gmail.com>
2019-12-08 03:28:46 -0500

    Updated .begin()

--- Client/client_app/client_app/process_messages.cpp
+++ Client/client_app/client_app/process_messages.cpp
@@ -3,7 +3,8 @@
 
 // void (json& db, const json& req_data) {
 
-void processMessages(json& db, vector<json> invitations_db, const json& req_data) {
+void processMessages(json& db, vector<json> invitations_db, const json& req_data)
+{
 	// scheduled that meeting is happening
 	// not_scheduled
 	// response unavailable
@@ -14,45 +15,54 @@ void processMessages(json& db, vector<json> invitations_db, const json& req_data
 	//meeting req_meeting = meeting::json_to_meetingObj(req_data);
 
 	//invite
-	if (!(messageType.invite.compare(req_data.at("message")))) {
-
-		meeting myMeeting = meeting(req_data.at("message"), "", "", req_data.at("meetingID"), {}, {}, "", req_data.at("topic"),
-			req_data.at("meetingDay"), req_data.at("meetingTime"), req_data.at("requesterIP"), false, false);
+	if (!(messageType.invite.compare(req_data.at("message"))))
+	{
+		meeting myMeeting = meeting(req_data.at("message"), "", "", req_data.at("meetingID"), {}, {}, "",
+		                            req_data.at("topic"),
+		                            req_data.at("meetingDay"), req_data.at("meetingTime"), req_data.at("requesterIP"),
+		                            false, false);
 		db_helper::add_invitation(myMeeting, invitations_db);
-
 	}
-	//confirm
-	else if (!(messageType.confirm.compare(req_data.at("message")))) {
-
+		//confirm
+	else if (!(messageType.confirm.compare(req_data.at("message"))))
+	{
 		meeting myMeeting = meeting();
 
-		if (db_helper::find_invitation(req_data.at("meetingID"), myMeeting, invitations_db)) {
+		if (db_helper::find_invitation(req_data.at("meetingID"), myMeeting, invitations_db))
+		{
 			db_helper::remove_invitation(req_data.at("meetingID"), invitations_db);
 
 			myMeeting.server_confirmation = true;
 			string roomNumber = req_data.at("roomNumber");
 			myMeeting.roomNumber = roomNumber;
 
-			meeting::update_meeting(db, myMeeting.meetingDay, myMeeting.meetingTime, meeting::meetingObj_to_json(myMeeting));
+			meeting::update_meeting(db, myMeeting.meetingDay, myMeeting.meetingTime,
+			                        meeting::meetingObj_to_json(myMeeting));
 			db_helper::save_db(config.DB_PATH, db);
 		}
 	}
-	//cancel
-	else if (!(messageType.cancelResponse.compare(req_data.at("message")))) {
+		//cancel
+	else if (!(messageType.cancelResponse.compare(req_data.at("message"))))
+	{
 		meeting myMeeting = meeting();
-		if (db_helper::find_invitation(req_data.at("meetingID"), myMeeting, invitations_db)) {
+		if (db_helper::find_invitation(req_data.at("meetingID"), myMeeting, invitations_db))
+		{
 			db_helper::remove_invitation(req_data.at("meetingID"), invitations_db);
 			//cout << endl << "----------" << "Meeting " << req_data.at("meetingID") << " cancelled." << endl <<
-				//"Number of participants is lower than minimum required." << endl;
+			//"Number of participants is lower than minimum required." << endl;
 			//TODO log Meeting Cancelled
 		}
 	}
-	//response unavailable
-	else if (!(messageType.response.compare(req_data.at("message")))) {
+		//response unavailable
+	else if (!(messageType.response.compare(req_data.at("message"))))
+	{
 		string requestId = req_data.at("requestID");
-		for (auto day : db) {
-			for (auto time : day) {
-				if (!requestId.compare(time.at("requestID"))) {
+		for (auto day : db)
+		{
+			for (auto time : day)
+			{
+				if (!requestId.compare(time.at("requestID")))
+				{
 					meeting::update_meeting(db, time.at("meetingDay"), time.at("meetingTime"), json({}));
 					db_helper::save_db(config.DB_PATH, db);
 					//TODO  log Request denied
@@ -61,17 +71,22 @@ void processMessages(json& db, vector<json> invitations_db, const json& req_data
 					break;
 				}
 			}
-			if (exit) {
+			if (exit)
+			{
 				break;
 			}
 		}
 	}
-	//Meeting Not Scheduled
-	else if (!(messageType.notScheduled.compare(req_data.at("message")))) {
+		//Meeting Not Scheduled
+	else if (!(messageType.notScheduled.compare(req_data.at("message"))))
+	{
 		string requestId = req_data.at("requestID");
-		for (auto day : db) {
-			for (auto time : day) {
-				if (!requestId.compare(time.at("requestID"))) {
+		for (auto day : db)
+		{
+			for (auto time : day)
+			{
+				if (!requestId.compare(time.at("requestID")))
+				{
 					meeting db_meeting = meeting::json_to_meetingObj(time);
 
 					for (const string element : req_data["confirmedParticipantsIP"])
@@ -86,17 +101,22 @@ void processMessages(json& db, vector<json> invitations_db, const json& req_data
 					break;
 				}
 			}
-			if (exit) {
+			if (exit)
+			{
 				break;
 			}
 		}
 	}
-	//Meeting scheduled successful
-	else if (!messageType.scheduled.compare(req_data.at("message"))) {
+		//Meeting scheduled successful
+	else if (!messageType.scheduled.compare(req_data.at("message")))
+	{
 		string requestId = req_data.at("requestID");
-		for (auto day : db) {
-			for (auto time : day) {
-				if (!requestId.compare(time.at("requestID"))) {
+		for (auto day : db)
+		{
+			for (auto time : day)
+			{
+				if (!requestId.compare(time.at("requestID")))
+				{
 					meeting thisMeeting = meeting::json_to_meetingObj(time);
 					string meetingId = req_data.at("meetingID");
 					string roomNumbr = req_data.at("roomNumber");
@@ -110,42 +130,52 @@ void processMessages(json& db, vector<json> invitations_db, const json& req_data
 
 					//TODO log schedule successful
 
-					meeting::update_meeting(db, thisMeeting.meetingDay, thisMeeting.meetingTime, meeting::meetingObj_to_json(thisMeeting));
+					meeting::update_meeting(db, thisMeeting.meetingDay, thisMeeting.meetingTime,
+					                        meeting::meetingObj_to_json(thisMeeting));
 					db_helper::save_db(config.DB_PATH, db);
-
 				}
 			}
 		}
 	}
-	//Added Participant to Meeting
-	else if (!messageType.added.compare(req_data.at("message"))) {
+		//Added Participant to Meeting
+	else if (!messageType.added.compare(req_data.at("message")))
+	{
 		string meetingId = req_data.at("meetingID");
-		for (auto day : db) {
-			for (auto time : day) {
-				if (!meetingId.compare(time.at("meetingID"))) {
+		for (auto day : db)
+		{
+			for (auto time : day)
+			{
+				if (!meetingId.compare(time.at("meetingID")))
+				{
 					meeting thisMeeting = meeting::json_to_meetingObj(time);
 
 					thisMeeting.confirmedParticipantsIP.push_back(req_data.at("IP"));
-					meeting::update_meeting(db, thisMeeting.meetingDay, thisMeeting.meetingTime, meeting::meetingObj_to_json(thisMeeting));
+					meeting::update_meeting(db, thisMeeting.meetingDay, thisMeeting.meetingTime,
+					                        meeting::meetingObj_to_json(thisMeeting));
 					db_helper::save_db(config.DB_PATH, db);
 					exit = true;
-
 				}
-				if (exit) {
+				if (exit)
+				{
 					break;
 				}
 			}
-			if (exit) {
+			if (exit)
+			{
 				break;
 			}
 		}
 	}
-	//Withdraw from Meeting (Requester side)
-	else if (!messageType.withdrawNotify.compare(req_data.at("message"))) {
+		//Withdraw from Meeting (Requester side)
+	else if (!messageType.withdrawNotify.compare(req_data.at("message")))
+	{
 		string meetingId = req_data.at("meetingID");
-		for (auto day : db) {
-			for (auto time : day) {
-				if (!meetingId.compare(time.at("meetingID"))) {
+		for (auto day : db)
+		{
+			for (auto time : day)
+			{
+				if (!meetingId.compare(time.at("meetingID")))
+				{
 					meeting thisMeeting = meeting::json_to_meetingObj(time);
 
 					int counter = -1;
@@ -153,51 +183,61 @@ void processMessages(json& db, vector<json> invitations_db, const json& req_data
 					for (const string element : thisMeeting.confirmedParticipantsIP)
 					{
 						counter += 1;
-						if (!element.compare(req_data.at("IP"))) {
-
-							thisMeeting.confirmedParticipantsIP.erase(thisMeeting.confirmedParticipantsIP.begin + counter);
-							meeting::update_meeting(db, thisMeeting.meetingDay, thisMeeting.meetingTime, meeting::meetingObj_to_json(thisMeeting));
+						if (!element.compare(req_data.at("IP")))
+						{
+							thisMeeting.confirmedParticipantsIP.erase(
+								thisMeeting.confirmedParticipantsIP.begin() + counter);
+							meeting::update_meeting(db, thisMeeting.meetingDay, thisMeeting.meetingTime,
+							                        meeting::meetingObj_to_json(thisMeeting));
 							db_helper::save_db(config.DB_PATH, db);
 							exit = true;
 						}
-						if (exit) {
+						if (exit)
+						{
 							break;
 						}
 					}
 				}
-				if (exit) {
+				if (exit)
+				{
 					break;
 				}
 			}
-			if (exit) {
+			if (exit)
+			{
 				break;
 			}
 		}
 	}
-	//Room_Change
-	else if (!messageType.roomChange.compare(req_data.at("message"))) {
+		//Room_Change
+	else if (!messageType.roomChange.compare(req_data.at("message")))
+	{
 		string meetingId = req_data.at("meetingID");
-		for (auto day : db) {
-			for (auto time : day) {
-				if (!meetingId.compare(time.at("meetingID"))) {
+		for (auto day : db)
+		{
+			for (auto time : day)
+			{
+				if (!meetingId.compare(time.at("meetingID")))
+				{
 					meeting thisMeeting = meeting::json_to_meetingObj(time);
 
 					string newRoom = req_data.at("newRoom");
 					thisMeeting.roomNumber = newRoom;
 
-					meeting::update_meeting(db, thisMeeting.meetingDay, thisMeeting.meetingTime, meeting::meetingObj_to_json(thisMeeting));
+					meeting::update_meeting(db, thisMeeting.meetingDay, thisMeeting.meetingTime,
+					                        meeting::meetingObj_to_json(thisMeeting));
 					db_helper::save_db(config.DB_PATH, db);
 					exit = true;
 				}
-				if (exit) {
+				if (exit)
+				{
 					break;
 				}
 			}
-			if (exit) {
+			if (exit)
+			{
 				break;
 			}
 		}
 	}
-
-
 }
